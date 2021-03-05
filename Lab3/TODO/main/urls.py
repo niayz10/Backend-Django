efc0c64e7ed0387894718399ed66f1a1060a1c0e
niayz10 <niayz10@mail.ru>
2021-03-05 22:33:08 +0600

    commit

--- Lab3/TODO/main/urls.py
+++ Lab3/TODO/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 
-urlpatterns = {
+urlpatterns = [
     path('todos', todo_list, name='todo_list'),
     path('todos/1/completed', complited_todo_list, name='complited_todo_list'),
 
-}
+]
--- Lab4/TODO/main/urls.py
+++ Lab4/TODO/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 
-urlpatterns = {
+urlpatterns = [
     path('todos', todo_list),
     path('todos/<int:id>/completed', complited_todo_list),
-}
+]
